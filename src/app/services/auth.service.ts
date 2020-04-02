fd0ec05c633f2de3282537fa7d4841635ebd5407
majkl zumberi <majklzumberi00@gmail.com>
2020-04-02 22:01:18 +0200

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
-
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  user:Observable<firebase.User>;
+  constructor(private afAuth:AngularFireAuth,private router:Router) { }
 
-  constructor() { }
+
+  signUp(email:string,password:string){
+    this.afAuth.createUserWithEmailAndPassword(email,password).then(
+      value=>{
+        console.log(`Success!, ${value}`);
+        //sessionStorage.setItem("user",value.user.uid);
+        //this.router.navigateByUrl('/home');
+      }
+    ).catch( err =>{
+      console.log(`Something went wrong: ${err.message}`);
+    })
+  }
+
+  signIn(){
+
+  }
 }
