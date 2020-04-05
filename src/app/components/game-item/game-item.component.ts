e2d048ab85ecca9cdd76515df0f98b91ca84f936
majkl zumberi <majklzumberi00@gmail.com>
2020-04-05 17:41:31 +0200

    show modifica link  if the user is admin

--- src/app/components/game-item/game-item.component.ts
+++ src/app/components/game-item/game-item.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit,Input,EventEmitter,Output} from '@angular/core';
 import { GameInterface } from 'src/app/models/GameInterface';
 import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/it';
+import { AngularFireAuth } from '@angular/fire/auth/auth';
+import { AuthService } from 'src/app/services/auth.service';
 registerLocaleData(localeFr, 'it');
 @Component({
   selector: 'app-game-item',
@@ -14,9 +16,13 @@ export class GameItemComponent implements OnInit {
   @Input() showRead:boolean=true;
   @Input() showForm:boolean=false;
   @Output() outputToEditGame=new EventEmitter<GameInterface>();
-  constructor() { }
+  showEdit:boolean=false;
+  constructor(private afAuth:AuthService) { }
 
   ngOnInit(): void {
+    this.afAuth.user$.subscribe(user=>{
+      this.showEdit=user.role === 'admin'? true:false;
+    });
   }
   tranferGameToForm(gameItemToForm:GameInterface){
     console.log("sto per passare al parent il gioco da modificare "+gameItemToForm.name);
