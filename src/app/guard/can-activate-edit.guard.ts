36f4ce58c1a5df790888aca56664687882e81916
majkl zumberi <majklzumberi00@gmail.com>
2020-04-06 12:57:40 +0200

    utilizzo map in can ativate edit per vedere ruolo admin utilizzo di pipe

--- src/app/guard/can-activate-edit.guard.ts
+++ src/app/guard/can-activate-edit.guard.ts
@@ -1,26 +1,43 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Route, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { AuthService } from '../services/auth.service';
+import {
+  Injectable
+} from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Route,
+  Router
+} from '@angular/router';
+import {
+  Observable
+} from 'rxjs';
+import {
+  AuthService
+} from '../services/auth.service';
+import {
+  map
+} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CanActivateEditGuard implements CanActivate {
-  constructor(private afAuth:AuthService,private route:Router){ }
+  constructor(private afAuth: AuthService, private route: Router) {}
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      this.afAuth.user$.subscribe(user=>{
-        console.log("da guard");
-        console.log(user.role);
-        if(user.role != "admin"){
-          this.route.navigateByUrl("/home");
-          return false;
-          
-        }
-      });
-      return true;
+    state: RouterStateSnapshot): Observable < boolean | UrlTree > | Promise < boolean | UrlTree > | boolean | UrlTree {
+    return this.afAuth.user$.pipe(
+      map(user => {
+          console.log("da guard");
+          console.log(user.role);
+          if (user.role != "admin") {
+            this.route.navigateByUrl("/home");
+            return false;
+          } else
+            return true;
+        })
+    );
   }
-  
+
 }
