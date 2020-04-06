41f020aedf7834bbabd9c1c981142f4d2d8b2332
majkl zumberi <majklzumberi00@gmail.com>
2020-04-06 10:22:41 +0200

    gestione lettura dataNascita con try catch

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,53 +1,87 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/services/auth.service';
-import { User } from 'src/app/models/userModel';
-import {NgForm, FormGroup, FormBuilder, Validators, FormControl} from '@angular/forms';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  AuthService
+} from 'src/app/services/auth.service';
+import {
+  User
+} from 'src/app/models/userModel';
+import {
+  NgForm,
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl
+} from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  editDataUserForm:FormGroup;
-  name:string="Majkl";
-  utente:User={
-    uid:'',
-    email:'',
-    displayName:'',
-    role:'',
-    birthDate:null,
-    male:null
+  editDataUserForm: FormGroup;
+  name: string = "Majkl";
+  utente: User = {
+    uid: '',
+    email: '',
+    displayName: '',
+    role: '',
+    birthDate: null,
+    male: null
   }
-  get dateControl():FormControl{
+  get dateControl(): FormControl {
     return this.editDataUserForm.get('data') as FormControl;
   }
-  get genderControl():FormControl{
+  get genderControl(): FormControl {
     return this.editDataUserForm.get('gender') as FormControl;
   }
-  constructor(public auth:AuthService,private fb:FormBuilder) { 
+  constructor(public auth: AuthService, private fb: FormBuilder) {
     let userToForm;
-   this.auth.user$.subscribe(user=>{
-      userToForm=user;
-      this.editDataUserForm=this.fb.group({
-        emailUser:[{value:user.email,disabled:true}],
-        username:[{value:user.displayName,disabled:true}],
-        data:new Date(user.birthDate).toISOString().substring(0, 10),
-        gender:user.male,
+    this.auth.user$.subscribe(user => {
+      userToForm = user;
+      let dataNascita = user.birthDate;
+      
+
+      try {
+         dataNascita = new Date(dataNascita);
+        /* Ends up logging `Invalid Date`
+         */
+
+        console.log(dataNascita);
+      } catch (_error) {
+        
+        /* Should come here and log `Error: Invalid Date` or the likes
+         */
+        console.log("data invalida quindi lascio quella di default");
+        console.log("Erorr: " + _error);
+      }
+      this.editDataUserForm = this.fb.group({
+        emailUser: [{
+          value: user.email,
+          disabled: true
+        }],
+        username: [{
+          value: user.displayName,
+          disabled: true
+        }],
+        data: dataNascita.toISOString().substring(0, 10),
+        gender: user.male,
       });
     })
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  updateUser(){
-    this.auth.user$.subscribe(user=>{
+  updateUser() {
+    this.auth.user$.subscribe(user => {
       console.log(user);
       console.log(this.genderControl.value);
       console.log(this.dateControl.value);
-      this.utente=user;
-      this.utente.male=this.genderControl.value;
-      this.utente.birthDate=this.dateControl.value;
+      this.utente = user;
+      this.utente.male = this.genderControl.value;
+      this.utente.birthDate = this.dateControl.value;
       this.auth.updateUserData(this.utente);
       document.getElementById("closeModal").click();
     });
